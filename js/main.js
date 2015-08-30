457be0290c2ac2a8e6b1816c9ba5fef1059aeaee
sempjc <jc.semprit@hotmail.com>
2015-08-29 22:23:33 -0400

    Already make the game work

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
+function updateCard(htmlDOM, choice, cardArray){
+  var img = 'url('+cardArray[choice].get_currentFace()+')';
+  $('#'+choice).css({ "background": img, "background-size": "80px 80px" });
+}
 //Main function-----------------------------------
 $( document ).ready(function() {
   //Create an array of pair of images
   //This will be the image the player need to guess
-  var backFace = '/images/back.png';
-  var blankFace = '/images/blanck.png'
   var sourceIMG = [
-    '/images/apple.png',
-    '/images/css3.png',
-    '/images/octocat.png',
-    '/images/slack.png',
-    '/images/HTML5.png',
-    '/images/JavaScript.png',
+    "/images/apple.png",
+    "/images/css3.png",
+    "/images/octocat.png",
+    "/images/slack.png",
+    "/images/HTML5.png",
+    "/images/JavaScript.png",
   ];
+  var backFace = '/images/back.png';
+  var blankFace = '/images/blanck.png';
   //The for loop will fill the empty imageFaces array with a pair of image
   //Variable i will increment the index of imageFaces
   //Variable img will increment the index of sourceIMG
@@ -20,22 +24,31 @@ $( document ).ready(function() {
     imageFaces[i] = sourceIMG[img];       //First pair
     imageFaces[(i+1)] = sourceIMG[img];   //Second pair
     img++; //Increase sourceIMG index
-    console.log('work');
   }
   //Initialize the game logic and call the methods initializeCard
   //to configure the game card.
   var game = new JCLogic();
   game.initializeCard(imageFaces,backFace, blankFace);
+  //Initialize the game score
+  var score = new JCScore();
+  //Put all card face down
+  for (var i = 0; i < imageFaces.length; i++){
+    game.cardDeck[i].faceDOWN();
+    updateCard($('#'+i), i, game.cardDeck);
+  }
 
-  //Event listener
-  $('.card').click(function(event){
+  //Event listener when user click on card
+  $('.card').on('click',function(event){
     //Evaluate if the user already choose two cards
     //If Only one is selected (choiceCounter = 1)
     //then skip the evaluation
       if(game.get_choiceCounter() === 1){
         game.set_choice1(event.target.id);    //assign the id number to choice1
-        //flip up the card in the position of choice1
+        //flip up the card in the position of choice1 to updated currentFace
         game.cardDeck[game.get_choice1()].faceUP();
+        //Draw the currentFace
+        updateCard(this, game.get_choice1(), game.cardDeck);
+        // $(this).css({ "background": img1, "background-size": "80px 80px" });
         game.set_choiceCounter(2);    //Set choiceCounter to 2
       }
       //If player has already choose one and now is selecting
@@ -43,28 +56,35 @@ $( document ).ready(function() {
       else {
         game.set_choice2(event.target.id);  //assign to choice2 the value of id
         //evaluate if the to card are the same or not
-        //If not the same (result in true) then compare the 2 card
+        //If not the same then compare the 2 card
         if(game.get_choice1() !== game.get_choice2()){
-          //Flip up the second card
+          //Flip up the second card to updated currentFace
           game.cardDeck[game.get_choice2()].faceUP();
-          if(game.compare2Card()){
-            //Eliminate cardDeck
-            game.cardDeck[game.get_choice1()].set_cardActiveState(false);
-            game.cardDeck[game.get_choice2()].set_cardActiveState(false);
-            //Increase match
-          }
+          updateCard(this, game.get_choice2(), game.cardDeck);
+          //If the two card have the same image, then eliminate
+          if(game.cardDeck[game.get_choice1()].get_cardActiveState() && game.cardDeck[game.get_choice1()].get_cardActiveState()){
+            if(game.compare2Card()){
+              //Eliminate cardDeck
+              game.cardDeck[game.get_choice1()].set_cardActiveState(false);
+              game.cardDeck[game.get_choice2()].set_cardActiveState(false);
+              //Increase match
+              score.match_increase_by_one();
+              console.log('match increase by one');
+            }
         }
-        //Flip card down
-        game.cardDeck[game.get_choice1()].faceDOWN();
-        game.cardDeck[game.get_choice2()].faceDOWN();
+          score.movesincrease_by_one();  //increase score
+          console.log('move increase by one');
+          game.set_choiceCounter(1);    //Reset to 1 choiceCounter
+          //Flip card down
+          //Delay for 1 second
+          setTimeout(function (){
+            game.cardDeck[game.get_choice1()].faceDOWN();
+            updateCard(this, game.get_choice1(), game.cardDeck);
+            game.cardDeck[game.get_choice2()].faceDOWN();
+            updateCard(this, game.get_choice2(), game.cardDeck);
+          }, 1000);
+        }
       }
-
-    if(game.get_choice1() !== game.get_choice2()){
-      //increase moves
-    }
-    game.set_choiceCounter(0);    //Reset to 0 choiceCounter
   });
-
-
   console.log('finish load');
 });
