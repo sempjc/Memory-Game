0aafe23c0b249d66b11d8d31a580f5137ddcc1d0
sempjc <jc.semprit@hotmail.com>
2015-08-29 23:37:37 -0400

    Setting for heroku

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var app = express();
 
@@ -5,16 +6,10 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('public/index.html');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
